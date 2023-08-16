7f21ff07b90528a161505ed9487595549c00a418
tkivelits <101392929+tkivelits@users.noreply.github.com>
2023-08-16 18:00:55 +0300

    Update occupation.dart

--- lib/commons/lv/occupation.dart
+++ lib/commons/lv/occupation.dart
@@ -64,7 +64,7 @@ class _OccupationPageState extends State<OccupationPage> {
         child: Column(children: <Widget>[
           const ExpansionTile(
             title: Text(
-              'Īsi',
+              'Īsumā',
               style: TextStyle(fontSize: 20),
             ),
             controlAffinity: ListTileControlAffinity.leading,
@@ -73,28 +73,28 @@ class _OccupationPageState extends State<OccupationPage> {
                 title: Padding(
                   padding: EdgeInsets.only(left: 25.0),
                   child: Text(
-                      '•Evakuējies uz to valsts daļu, ko kontrolē NBS vai NATO apvienotie spēki'),
+                      '•Evakuējieties uz to valsts daļu, ko kontrolē NBS vai NATO apvienotie spēki'),
                 ),
                 visualDensity: VisualDensity(vertical: -4),
               ),
               ListTile(
                 title: Padding(
                   padding: EdgeInsets.only(left: 25.0),
-                  child: Text('•Turpini ievērot Latvijas Republikas likumus'),
+                  child: Text('•Turpiniet ievērot Latvijas Republikas likumus'),
                 ),
                 visualDensity: VisualDensity(vertical: -4),
               ),
               ListTile(
                 title: Padding(
                   padding: EdgeInsets.only(left: 25.0),
-                  child: Text('•Nesadarbojies ar okupācijas varu:'),
+                  child: Text('•Nesadarbojieties ar okupācijas varu:'),
                 ),
                 visualDensity: VisualDensity(vertical: -4),
               ),
               ListTile(
                 title: Padding(
                   padding: EdgeInsets.only(left: 50.0),
-                  child: Text('•Nošķiries no viņu lēmumiem un darbībām'),
+                  child: Text('•Nošķirieties no viņu lēmumiem un darbībām'),
                 ),
                 visualDensity: VisualDensity(vertical: -4),
               ),
@@ -102,7 +102,7 @@ class _OccupationPageState extends State<OccupationPage> {
                 title: Padding(
                   padding: EdgeInsets.only(left: 50.0),
                   child: Text(
-                      '•Nepiedalies to organizētajos publiskajos pasākumos, nesniedzot tiem intervijas un neļaujot sevi filmēt'),
+                      '•Nepiedalieties to organizētajos publiskajos pasākumos, nesniedzoiet tiem intervijas un neļaujiett sevi filmēt'),
                 ),
                 visualDensity: VisualDensity(vertical: -4),
               ),
@@ -110,7 +110,7 @@ class _OccupationPageState extends State<OccupationPage> {
                 title: Padding(
                   padding: EdgeInsets.only(left: 50.0),
                   child: Text(
-                      '•Nepiedalies nelikumīgās vēlēšanās, referendumos vai tautas nobalsošanās'),
+                      '•Nepiedalieties nelikumīgās vēlēšanās, referendumos vai tautas nobalsošanās'),
                 ),
                 visualDensity: VisualDensity(vertical: -4),
               ),
@@ -165,9 +165,9 @@ class _OccupationPageState extends State<OccupationPage> {
                 child: Padding(
                   padding: const EdgeInsets.only(left: 32.0, right: 8.0),
                   child: Text(
-                    "•Evakuējies uz to valsts daļu, ko kontrolē NBS\n"
-                    "•Turpini ievērot Latvijas Republikas likumus\n"
-                    "•Nesadarbojies ar okupācijas varu:",
+                    "•Evakuējieties uz to valsts daļu, ko kontrolē NBS\n"
+                    "•Turpiniet ievērot Latvijas Republikas likumus\n"
+                    "•Nesadarbojieties ar okupācijas varu:",
                     style: TextStyle(fontSize: 16),
                     textAlign: TextAlign.justify,
                   ),
@@ -178,10 +178,10 @@ class _OccupationPageState extends State<OccupationPage> {
                 child: Padding(
                   padding: const EdgeInsets.only(left: 48.0, right: 8.0),
                   child: Text(
-                    "•Noškiries no viņu lēmumiem un darbībām\n"
-                    "•Nepiedalies to organizētājos publiskajos pasākumos, nesniedzot tiem "
+                    "•Noškirieties no viņu lēmumiem un darbībām\n"
+                    "•Nepiedalieties to organizētājos publiskajos pasākumos, nesniedzot tiem "
                     "intervijas un neļaujot sevi filmēt\n"
-                    "•Nepiedalies nelikumīgās vēlēšanās, referendumos vai tautas nobalsošanās",
+                    "•Nepiedalieties nelikumīgās vēlēšanās, referendumos vai tautas nobalsošanās",
                     style: TextStyle(fontSize: 16),
                     textAlign: TextAlign.justify,
                   ),
@@ -192,7 +192,7 @@ class _OccupationPageState extends State<OccupationPage> {
                 child: Padding(
                   padding: const EdgeInsets.only(left: 16.0, right: 8.0),
                   child: Text(
-                    "Ja izvēlies pretoties, tev ir tiesības:",
+                    "Ja izvēlieties pretoties, jums ir tiesības:",
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     textAlign: TextAlign.left,
                   ),
@@ -234,16 +234,16 @@ class _OccupationPageState extends State<OccupationPage> {
               Padding(
                 padding: const EdgeInsets.only(left: 32.0, right: 8.0),
                 child: Text(
-                  "•Uzmanīgi ieklausies paziņojumā par evakuācijas norisi, iegaumē un piefiksē evakuācijas "
-                  "virzienu, galamērki vai pulcēšanās vietu.\n"
-                  "•Visur līdzi nem savu ID vai pasi.\n"
-                  "•Ja vari, pirms evakuācijas atslēdz savam mājoklim gāzes, elektrības un "
-                  "ūdens padevi, aiztaisi logus, aizslēdz durvis.\n"
-                  "•Izmanto maršrutus, kurus rekomendē dienesti un nepārvietojies pa nepazīstamiem celiem.\n"
-                  "•Ja nav iespējams izmantot savu transportlīdzekli, dodies uz tuvāko norādīto"
+                  "•Uzmanīgi ieklausieties paziņojumā par evakuācijas norisi, iegaumējiet un piefiksējiet evakuācijas "
+                  "virzienu, galamērķi vai pulcēšanās vietu.\n"
+                  "•Visur līdzi ņemiet savu ID vai pasi.\n"
+                  "•Ja variet, pirms evakuācijas atslēdziet savām mājoklim gāzes, elektrības un "
+                  "ūdens padevi, aiztaisiet logus, aizslēdziet durvis.\n"
+                  "•Izmantojiet maršrutus, kurus rekomendē dienesti un nepārvietojieties pa nepazīstamiem celiem.\n"
+                  "•Ja nav iespējams izmantot savu transportlīdzekli, dodieties uz tuvāko norādīto"
                   " evakuācijas pulcēšanās vietu.\n"
-                  "•Evakuācijas gadījumā paziņo par to tuviniekiem.\n"
-                  "•Ņem līdzi ārkārtas gadījumu somu.",
+                  "•Evakuācijas gadījumā paziņojiet par to tuviniekiem.\n"
+                  "•Ņemiet līdzi ārkārtas gadījumu somu.",
                   style: TextStyle(fontSize: 16),
                   textAlign: TextAlign.justify,
                 ),
@@ -253,7 +253,7 @@ class _OccupationPageState extends State<OccupationPage> {
                 child: Padding(
                   padding: const EdgeInsets.only(left: 16.0, right: 8.0),
                   child: Text(
-                    "Ņem vērā:",
+                    "Ņemiet vērā:",
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     textAlign: TextAlign.justify,
                   ),
@@ -262,13 +262,13 @@ class _OccupationPageState extends State<OccupationPage> {
               Padding(
                 padding: EdgeInsets.only(left: 32.0, right: 8.0, bottom: h),
                 child: Text(
-                  "•Necenties atgriezties mājās, kamēr atbildīgie dienesti nav to atļāvuši\n"
-                  "•Nevalkā paramilitāru apģērbu, kamuflāžas formu vai militarizētas somasja neesi dienestā.\n"
-                  "•Bez vajadzības netuvojies, neprovocē un nefilmē neidentificētus bruņotus cilvēkus, "
-                  "nepacel no zemes pamestus ieročus vai aizdomīgus priekšmetus.\n"
-                  "•Nemēģini paša spēkiem pārvietot nesprāgušos lādiņus, mīnas vai atspējot "
+                  "•Necentieties atgriezties mājās, kamēr atbildīgie dienesti nav to atļāvuši\n"
+                  "•Nevelciet paramilitāru apģērbu, kamuflāžas formu vai militarizētas somas, ja neesat dienestā.\n"
+                  "•Bez vajadzības netuvojieties, neprovocējiet un nefilmējiet neidentificētus bruņotus cilvēkus, "
+                  "nepaceliet no zemes pamestus ieročus vai aizdomīgus priekšmetus.\n"
+                  "•Nemēģiniet paša spēkiem pārvietot nesprāgušos lādiņus, mīnas vai atspējot "
                   "improvizētos spridzekļus.\n"
-                  "•Neizplati informāciju (fotogrāfijas, video, tekstus) par Nacionālajiem bruņotajiem "
+                  "•Neizplatiet informāciju (fotogrāfijas, video, tekstus) par Nacionālajiem bruņotajiem "
                   "spēkiem un sabiedroto spēku pārvietošanos, atrašanās vietām un pozīcijām.",
                   style: TextStyle(fontSize: 16),
                   textAlign: TextAlign.justify,
